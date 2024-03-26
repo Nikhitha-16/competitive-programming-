9f5b990d223a11af02a25346212648d0ce7886d4
Nikhitha-16 <163624684+Nikhitha-16@users.noreply.github.com>
2024-03-26 14:47:50 +0530

    Add files via upload

---
+++ remove nodes from linked list.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def removeNode (head):
+            if (head == None):
+                return None
+            head.next = removeNode(head.next);
+            return head.next if (head.next != None and head.val < head.next.val) else head;
+        return removeNode(head) 
